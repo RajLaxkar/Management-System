501f2a07b5510e6d4165100b88e458877488a3ae
RajLaxkar <115140554+RajLaxkar@users.noreply.github.com>
2025-03-16 15:49:06 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -31,10 +31,3 @@ class Employee(models.Model):
 
 
 
-# Matlab agar aap kisi employee ka department ya role check karna chahein, toh uska reference Department aur Role tables me milega.
-# on_delete=models.CASCADE ka Matlab
-# on_delete batata hai ki jab ek parent record (Department ya Role) delete ho jaye, toh uske associated child records (Employee) par kya effect padega.
-# models.CASCADE
-# Agar koi Department delete hota hai, toh us department ke saare employees bhi delete ho jayenge.
-# Example: dept = models.ForeignKey(Department, on_delete=models.CASCADE)
-# Agar "HR" department delete ho gaya, toh jitne bhi employees HR department me kaam kar rahe the, wo bhi delete ho jayenge.
